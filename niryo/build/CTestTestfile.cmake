b75cc04a620c5ba5059f49ef54b9bbbfda25107c
abrandi@codearchitects.com <abrandi@codearchitects.com>
2021-02-09 13:21:38 +0100

    removed useless build files

--- niryo/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omncbar/Nyrio_Software/niryo/src
-# Build directory: /home/omncbar/Nyrio_Software/niryo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("niryo_description")
-subdirs("niryo_gazebo")
--- niryo/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gmock
-# Build directory: /home/omncbar/Nyrio_Software/niryo/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
