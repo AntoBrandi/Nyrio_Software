6dccb995fb4fa125198d4f286d82842089faba09
abrandi@codearchitects.com <abrandi@codearchitects.com>
2021-02-09 13:19:58 +0100

    created workspace

---
+++ niryo/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/omncbar/Nyrio_Software/niryo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/omncbar/mastering-ros/mastering_ros_ws/devel;/home/omncbar/mobile-manipulator/mobile_manipulator_ws/devel;/home/omncbar/arduinobot/arduinobot_ws/devel;/home/omncbar/ros_tutorial/devel;/home/omncbar/aptius2/devel;/opt/ros/kinetic'.split(';') if '/home/omncbar/mastering-ros/mastering_ros_ws/devel;/home/omncbar/mobile-manipulator/mobile_manipulator_ws/devel;/home/omncbar/arduinobot/arduinobot_ws/devel;/home/omncbar/ros_tutorial/devel;/home/omncbar/aptius2/devel;/opt/ros/kinetic' != '' else []
---
+++ niryo/build/niryo_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/omncbar/Nyrio_Software/niryo/src/niryo_gazebo
+# Build directory: /home/omncbar/Nyrio_Software/niryo/build/niryo_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
